6bc519abfa74793e59b85b9a910ff402d4824f5c
yacoubou-seidou <yacoseidou@gmail.com>
2023-07-18 12:06:35 +0100

    update the view and add edit functionality

--- script.js
+++ script.js
@@ -22,17 +22,19 @@ tabBtn.addEventListener("click", function () {
     myLeads.push(lead);
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
     render(myLeads);
+    deleteFunction();
+    editFunction();
+    toggleEdit()
   });
 });
 
 function render(myLeads) {
   const element = document.querySelector('.listContent');
-  console.log(myLeads);
   const filteredArray = myLeads.sort((a, b) => a.index - b.index);
   let content = "";
-  filteredArray.forEach((lead) => {
+  filteredArray.forEach((lead, index) => {
     content += `
-    <li class='borderStyle d-flex justify-content-center align-items-center col-12'>
+    <li class='borderStyle d-flex justify-content-center align-items-center col-12' data-iden="${index}">
     <fieldset>
     <legend> <a href='${lead.link}' class='leLien' target="_blank" rel="noopener noreferrer">
     ${lead.showName.substring(0, 18)}</a ></legend >
@@ -48,20 +50,21 @@ function render(myLeads) {
   element.innerHTML = content;
 
 }
-const listContent = document.querySelector('.listContent');
-const deleteFunction = () => {
+function deleteFunction() {
   const supp = document.querySelectorAll(".spanbtn");
   supp.forEach((element) => {
     element.addEventListener("click", () => {
-      let idx = parseInt(element.parentNode.getAttribute("data-iden"));
+      console.log(element.parentNode.parentNode.parentNode);
+      let idx = parseInt(element.parentNode.parentNode.parentNode.getAttribute("data-iden"));
+      console.log(idx);
       deleteTask(myLeads, idx)
-      let removeEl = element.parentNode.parentNode;
+      let removeEl = element.parentNode.parentNode.parentNode;
       removeEl.remove();
     });
   });
 };
 
-const editFunction = () => {
+function editFunction() {
   const formsElements = document.querySelectorAll('.formel');
   formsElements.forEach((formel, index) => {
     formel.addEventListener('input', (e) => {
@@ -79,7 +82,7 @@ const isLink = (value) => {
   var urlPattern = /^(https?:\/\/)?([\w.-]+)\.([a-z]{2,})(\/[\w.-]*)*\/?$/i;
   return urlPattern.test(value);
 }
-const toggleInput = (id) => {
+function toggleInput(id) {
   const billingItems = document.querySelectorAll('.formel');
   billingItems[id].classList.toggle('edition');
   if (billingItems[id].classList.contains('edition')) {
@@ -88,12 +91,11 @@ const toggleInput = (id) => {
     editFunction();
   }
 }
-const toggleEdit = () => {
+function toggleEdit() {
 
   const edittable = document.querySelectorAll('.Edittable');
   edittable.forEach((edit, index) => {
     edit.addEventListener('click', () => {
-      console.log(index);
       toggleInput(index)
       deleteFunction();
       editFunction();
@@ -106,9 +108,9 @@ inputBtn.addEventListener("click", function () {
     addNewlink(myLeads, inputEl.value)
     render(myLeads);
     let element = '';
-    myLeads.forEach((lead) => {
+    myLeads.forEach((lead, index) => {
       element += `
-    <li class='borderStyle d-flex justify-content-center align-items-center col-12'>
+    <li class='borderStyle d-flex justify-content-center align-items-center col-12' data-iden="${index}">
     <fieldset>
     <legend> <a href='${lead.link}' class='leLien' target="_blank" rel="noopener noreferrer">
     ${lead.showName.substring(0, 18)}</a ></legend >
